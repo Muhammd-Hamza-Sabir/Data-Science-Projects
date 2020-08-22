9e6debcf6e6fc8948ca6d60d6a22e4edad6ab5d3
Muhammad-Hamza-Sabir <muhammadhamzasabir752@gmail.com>
2020-08-22 16:51:47 +0500

    Handling Imbalance Dataset & Modeling

--- Titanic/2. Handling Imbalance Dataset.py
+++ Titanic/2. Handling Imbalance Dataset.py
@@ -13,7 +13,7 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 import seaborn as sns
 
 
-# In[2]:
+# In[19]:
 
 
 train = pd.read_csv(r'./titanic/train_set.csv')
@@ -56,32 +56,32 @@ train_x.shape, train_y.shape
 
 # ### 1.1 Data Scaling & Splitting
 
-# In[8]:
+# In[21]:
 
 
 scaler = StandardScaler()
 scaled_train_x = scaler.fit_transform(train_x)
 
 
-# In[9]:
+# In[22]:
 
 
 scaled_train_x
 
 
-# In[10]:
+# In[23]:
 
 
 train_set, test_set, train_label, test_label = train_test_split(scaled_train_x, train_y, test_size=0.1, shuffle=True)
 
 
-# In[11]:
+# In[24]:
 
 
 train_set.shape, train_label.shape
 
 
-# In[12]:
+# In[25]:
 
 
 test_set.shape, test_label.shape
@@ -89,7 +89,7 @@ test_set.shape, test_label.shape
 
 # ### 1.2 Data Modeling
 
-# In[13]:
+# In[26]:
 
 
 # Data Modeling
@@ -100,92 +100,92 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 
-# In[19]:
+# In[27]:
 
 
 models = [LogisticRegression, DecisionTreeClassifier, RandomForestClassifier, AdaBoostClassifier, BaggingClassifier,          GradientBoostingClassifier, SVC]
 
 
-# In[34]:
+# In[46]:
 
 
 scores = {}
 for model in models:
-    model = model()
+    model = model(random_state=50)
     model.fit(X=train_set, y=train_label)
     predictions = model.predict(test_set)
     scores[model.__class__.__name__] = {'precision': precision_score(test_label, predictions, average='micro'),                                       'recall': recall_score(test_label, predictions, average='binary'),                                       'accuracy': accuracy_score(test_label, predictions)}
 
 
-# In[35]:
+# In[47]:
 
 
 scores
 
 
-# In[36]:
+# In[48]:
 
 
 models_score = pd.DataFrame(scores.values(), index=scores.keys())
 
 
-# In[37]:
+# In[49]:
 
 
 models_score
 
 
-# <p>Highest Recall achieved so far is 72% by <b>BaggingClassifier</b>. The reason of not achieving good recall is the imbalance dataset. We will be handling this out in next steps. As we know data is imbalanced, that's why accuracy is not reliable even achieved about 80%.</p>
+# <p>Highest Recall achieved so far is 75% by <b>LogisticRegression & AdaBoostClassifier</b>. The reason of not achieving good recall is the imbalance dataset. We will be handling this out in next steps. As we know data is imbalanced, that's why accuracy is not reliable even achieved about 80%.</p>
 
 # ## 2. Imbalance Dataset
 
-# In[45]:
+# In[50]:
 
 
 train['Survived'].value_counts()
 
 
-# ### 2.1 Under Sampling
+# ### 2.1 Random Under Sampling
 
-# In[64]:
+# In[51]:
 
 
-train_0 = train[train['Survived']==0].sample(n=263, replace=False)
+train_0 = train[train['Survived']==0].sample(n=263, replace=False, random_state=42)
 train_1 = train[train['Survived']==1]
 
 
-# In[67]:
+# In[52]:
 
 
 train_ = pd.concat([train_0, train_1])
 train_.reset_index(drop=True, inplace=True)
 
 
-# In[69]:
+# In[53]:
 
 
 train_.shape
 
 
-# In[73]:
+# In[54]:
 
 
 train_ = train_.sample(frac=1, random_state=42).reset_index(drop=True)
 
 
-# In[75]:
+# In[55]:
 
 
 train_.head()
 
 
-# In[76]:
+# In[56]:
 
 
 train_.tail()
 
 
-# In[80]:
+# In[57]:
 
 
 cols = list(train_.columns)[1:]
@@ -193,38 +193,47 @@ train_x = train_.loc[:,cols]
 train_y = train_.loc[:,'Survived']
 
 
-# In[83]:
+# In[58]:
+
+
+fig, ax = plt.subplots(figsize=(10, 7))
+sns.countplot('Survived', data=train_)
+ax.set_title('Counter for each class')
+plt.show()
+
+
+# In[59]:
 
 
 train_x.shape, train_y.shape
 
 
-# In[84]:
+# In[60]:
 
 
 scaler = StandardScaler()
 scaled_train_x = scaler.fit_transform(train_x)
 
 
-# In[85]:
+# In[61]:
 
 
 train_set, test_set, train_label, test_label = train_test_split(scaled_train_x, train_y, test_size=0.1, shuffle=True)
 
 
-# In[88]:
+# In[62]:
 
 
 train_set.shape, train_label.shape
 
 
-# In[89]:
+# In[63]:
 
 
 test_set.shape, test_label.shape
 
 
-# In[90]:
+# In[64]:
 
 
 # Data Modeling
@@ -235,42 +244,42 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 
-# In[91]:
+# In[65]:
 
 
 models = [LogisticRegression, DecisionTreeClassifier, RandomForestClassifier, AdaBoostClassifier, BaggingClassifier,          GradientBoostingClassifier, SVC]
 
 
-# In[92]:
+# In[68]:
 
 
 scores = {}
 for model in models:
-    model = model()
+    model = model(random_state=50)
     model.fit(X=train_set, y=train_label)
     predictions = model.predict(test_set)
     scores[model.__class__.__name__] = {'precision': precision_score(test_label, predictions, average='micro'),                                       'recall': recall_score(test_label, predictions, average='binary'),                                       'accuracy': accuracy_score(test_label, predictions)}
 
 
-# In[93]:
+# In[69]:
 
 
 scores
 
 
-# In[94]:
+# In[70]:
 
 
 models_score_2 = pd.DataFrame(scores.values(), index=scores.keys())
 
 
-# In[95]:
+# In[71]:
 
 
 models_score_2
 
 
-# In[96]:
+# In[72]:
 
 
 models_score['precision_under_sample'] = models_score_2['precision']
@@ -278,16 +287,136 @@ models_score['recall_under_sample'] = models_score_2['recall']
 models_score['accuracy_under_sample'] = models_score_2['accuracy']
 
 
-# In[97]:
+# In[73]:
 
 
 models_score
 
 
-# <p>Highest Recall achieved after under sampling is <b>74%</b> which is 2% more than earlier. We will apply oversampling method to check if there is possibility to improve the model performance.</p>
+# <p>Highest Recall achieved after under sampling is <b>81%</b> which is 6% more than earlier. We will apply oversampling method to check if there is possibility to improve the model performance.</p>
 
-# In[ ]:
+# ### 2.2 Random Over Sampling
+
+# In[74]:
 
 
+from collections import Counter
+from imblearn.over_sampling import RandomOverSampler
 
 
+# In[85]:
+
+
+Counter(train["Survived"])
+
+
+# In[86]:
+
+
+OverSampler = RandomOverSampler(sampling_strategy='minority')
+
+
+# In[88]:
+
+
+train_set
+
+
+# In[92]:
+
+
+train_set_over, train_label_over = OverSampler.fit_sample(train.iloc[:,1:].values, train["Survived"].values)
+
+
+# In[93]:
+
+
+np.shape(train_set_over), np.shape(train_label_over)
+
+
+# In[94]:
+
+
+scaler = StandardScaler()
+scaled_train_x = scaler.fit_transform(train_set_over)
+
+
+# In[96]:
+
+
+train_set, test_set, train_label, test_label = train_test_split(train_set_over, train_label_over, test_size=0.1, shuffle=True)
+
+
+# In[97]:
+
+
+train_set.shape, train_label.shape
+
+
+# In[98]:
+
+
+test_set.shape, test_label.shape
+
+
+# In[99]:
+
+
+# Data Modeling
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier, BaggingClassifier, GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+
+# In[100]:
+
+
+models = [LogisticRegression, DecisionTreeClassifier, RandomForestClassifier, AdaBoostClassifier, BaggingClassifier,          GradientBoostingClassifier, SVC]
+
+
+# In[101]:
+
+
+scores = {}
+for model in models:
+    model = model(random_state=50)
+    model.fit(X=train_set, y=train_label)
+    predictions = model.predict(test_set)
+    scores[model.__class__.__name__] = {'precision': precision_score(test_label, predictions, average='micro'),                                       'recall': recall_score(test_label, predictions, average='binary'),                                       'accuracy': accuracy_score(test_label, predictions)}
+
+
+# In[102]:
+
+
+scores
+
+
+# In[103]:
+
+
+models_score_3 = pd.DataFrame(scores.values(), index=scores.keys())
+
+
+# In[104]:
+
+
+models_score_3
+
+
+# In[105]:
+
+
+models_score['precision_over_sample'] = models_score_3['precision']
+models_score['recall_over_sample'] = models_score_3['recall']
+models_score['accuracy_over_sample'] = models_score_3['accuracy']
+
+
+# In[106]:
+
+
+models_score
+
+
+# <p>Highest Recall achieved after under sampling is <b>90%</b> which is 9% more than earlier. Hence, shown that oversampling performs better than undersampling.</p>
